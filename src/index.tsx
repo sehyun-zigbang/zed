5074b7bf148ee7a425160809c041de38ce4e8cf6
sehyun <sehyun@zigbang.com>
2022-10-28 00:48:45 +0900

    hdr 추가 및 수치 조정

--- src/index.tsx
+++ src/index.tsx
@@ -35,11 +35,11 @@ const observerData: ObserverData = {
     lighting: {
         direct: 0.6,
         env: {
-            value: getAssetPath('./skybox/artist_workshop_2k.hdr'),
+            value: getAssetPath('./skybox/photo_studio_broadway_hall_2k.hdr'),
             options: null,
             default: null,
             skyboxMip: '0',
-            exposure: 0.2,
+            exposure: -1,
             backgroundColor: { r: 0.4, g: 0.45, b: 0.5 },
             rotation: 0
         },
@@ -52,7 +52,7 @@ const observerData: ObserverData = {
             rotation_y: 15,
             rotation_z: 0,
             shadow: true,
-            shadowResolution: 2048,
+            shadowResolution: 4096,
             shadowIntencity: 1
         },
         subLight: {
